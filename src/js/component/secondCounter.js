137b7cb636d6836ff8afd91aaf647413a8232550
Emi-A <emilyam23@gmail.com>
2021-03-09 01:26:21 +0000

    subiendo

---
+++ src/js/component/secondCounter.js
@@ -0,0 +1,74 @@
+import React from "react";
+//poner aqui los props de card <Card props={aux} />
+import PropTypes from "prop-types";
+//importar card
+import { CardTime } from "./card.js";
+
+//create your first component
+export function Home(props) {
+	return (
+		<div className="container mt-5">
+			<div className="row p-3 mb-2 bg-secondary text-white">
+				<div className="col-1 "></div>
+				<div className="col-1">
+					<h1>
+						<span className="badge bg-secondary border border-light">
+							<i className="fas fa-clock"></i>
+						</span>
+					</h1>
+				</div>
+				<div className="col-1">
+					<h1>
+						<span className="badge bg-secondary border border-light">
+							{props.dig5 % 10}
+						</span>
+					</h1>
+				</div>
+				<div className="col-1">
+					<h1>
+						<span className="badge bg-secondary border border-light">
+							{props.dig4 % 10}
+						</span>
+					</h1>
+				</div>
+				<div className="col-1">
+					<h1>
+						<span className="badge bg-secondary border border-light">
+							{props.dig3 % 10}
+						</span>
+					</h1>
+				</div>
+				<div className="col-1">
+					<h1>
+						<span className="badge bg-secondary border border-light">
+							{props.dig2 % 10}
+						</span>
+					</h1>
+				</div>
+				<div className="col-1">
+					<h1>
+						<span className="badge bg-secondary border border-light">
+							{props.dig1}
+						</span>
+					</h1>
+				</div>
+				<div className="col-1">
+					<h1>
+						<span className="badge bg-secondary border border-light">
+							{props.dig0 % 60}
+						</span>
+					</h1>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+Home.propTypes = {
+	dig0: PropTypes.number,
+	dig1: PropTypes.number,
+	dig2: PropTypes.number,
+	dig3: PropTypes.number,
+	dig4: PropTypes.number,
+	dig5: PropTypes.number
+};
--- src/js/index.js
+++ src/js/index.js
@@ -9,7 +9,35 @@ import "bootstrap";
 import "../styles/index.scss";
 
 //import your own components
-import { Home } from "./component/home.js";
+import { Home } from "./component/secondCounter.js";
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+let num0,
+	num1,
+	num2,
+	num3,
+	num4,
+	num5 = 0;
+let second_counter = 0;
+
+setInterval(function(props) {
+	num0 = Math.floor(second_counter / 1);
+	num1 = Math.floor(second_counter / 60);
+	num2 = Math.floor(second_counter / 1000);
+	num3 = Math.floor(second_counter / 1000);
+	num4 = Math.floor(second_counter / 10000);
+	num5 = Math.floor(second_counter / 10000);
+
+	second_counter++;
+
+	ReactDOM.render(
+		<Home
+			dig0={num0}
+			dig1={num1}
+			dig2={num2}
+			dig3={num3}
+			dig4={num4}
+			dig5={num5}
+		/>,
+		document.querySelector("#app")
+	);
+}, 1000);
